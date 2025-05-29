e30cd5b1a120dc583cf5ee6342411560961c7107
SamuelLindao <lacerdasamuel26@gmail.com>
2025-05-28 23:19:52 -0300

    methods

--- cap03/vectors.js
+++ cap03/vectors.js
@@ -4,10 +4,16 @@ const products = new Array()
 
 for(i = 0; i < 25; i++)
 {
-    products[i] = Math.random() * 1000
+    products.push(Math.random() * 1000)
 }
 
 for (i = 0; i < products.length; i++)
 {
     console.log(i + "\t"+ products[i])
 }
+
+products.slice(0,-1)//Pega de 0 a 3
+products.slice(-2) //Pega os 2 ultimos elementos
+
+products.shift()//remove o primeiro
+products.pop() //remove o ultimo
