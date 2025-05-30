24776b4a4f5b3bd97e0ca1c659b4f59ab328a900
SamuelLindao <lacerdasamuel26@gmail.com>
2025-05-29 22:03:06 -0300

    rest and vector objects

---
+++ cap03/objects.js
@@ -0,0 +1,12 @@
+const cars = []
+cars.push({modelo: "Sandero", preco: 450})
+cars.push({modelo: "Valor", preco: 42})
+const args = process.argv.slice(2);
+for(const car of cars) {
+    if(args.includes('modelos'))
+        console.log(car.modelo)
+    else
+        console.log(car.preco)
+}
+
+console.log("Desestruturação")
