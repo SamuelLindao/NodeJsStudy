31762c23d0f3632ea2efb757ecd8e6af1800f182
SamuelLindao <lacerdasamuel26@gmail.com>
2025-05-31 17:30:08 -0300

    variables on arrow function

--- cap03/functions.js
+++ cap03/functions.js
@@ -1,6 +1,8 @@
 //arrow function
 //Isso é chamado função de seta, pois cria uma função a partir de uma seta em uma variavel, muito util pqp
-const ola2 = ()=> {
-    console.log("Olá 2")
+const ola2 = (a)=> {
+    console.log(`Olá 2${a}`)
 }
-ola2()
+ola2(1)
+
+//funções eu não preciso ver, sou tchola é?
