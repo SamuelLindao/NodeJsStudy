8b6d9b2aa12dbcf59de8501bd7fe707a0faa6282
SamuelLindao <lacerdasamuel26@gmail.com>
2025-05-31 15:46:36 -0300

    string study

--- cap03/filters.js
+++ cap03/filters.js
@@ -1,5 +1,8 @@
 const numeros = [1,2,3,4,5,6]
 
 const dobros = numeros.map(num => num * 2)
-
+const values = numeros.filter(num => num % 2 === 0)
+const soma = numeros.reduce((acumulator, num)=> acumulator + num, 0)
+console.log(soma/numeros.length)
 console.log(dobros.join(', '))
+console.log(values.join(', '))
