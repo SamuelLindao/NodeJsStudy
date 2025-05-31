aa91039ab12ac6e7bea578b1df9e758910452d0e
SamuelLindao <lacerdasamuel26@gmail.com>
2025-05-31 17:29:18 -0300

    arrow function

---
+++ cap03/functions.js
@@ -0,0 +1,6 @@
+//arrow function
+//Isso é chamado função de seta, pois cria uma função a partir de uma seta em uma variavel, muito util pqp
+const ola2 = ()=> {
+    console.log("Olá 2")
+}
+ola2()
